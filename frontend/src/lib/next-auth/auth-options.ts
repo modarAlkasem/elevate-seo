931dcad0bdccd2fa128f51178673fdd61edf5564
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-09 23:30:36 +0200

    feat: Implement auth's error codes

---
+++ frontend/src/lib/next-auth/auth-options.ts
@@ -0,0 +1,8 @@
+/// <reference types="./next-auth.d.ts" />
+
+import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import type { GoogleProfile } from "next-auth/providers/google";
+
+import { DateTime } from "luxon";
