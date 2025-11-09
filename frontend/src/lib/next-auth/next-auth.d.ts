4510201f20680b07b738f7733932348c4532564b
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-09 23:14:19 +0200

    feat:override some NextAuth types

---
+++ frontend/src/lib/next-auth/next-auth.d.ts
@@ -0,0 +1,35 @@
+import type { AuthOptions } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id: number;
+    name: string | null;
+    email: string;
+    email_verified: boolean;
+    last_signed_in: string;
+    disabled: boolean;
+    picture: string | null;
+    access: string;
+    refresh: string;
+  }
+
+  interface Session {
+    user: Omit<User, "access" | "refresh">;
+    access: string;
+    refresh: string;
+    expires: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: number;
+    name: string;
+    email: string;
+    email_verified: boolean;
+    last_signed_in: string;
+    access: string;
+    refresh: string;
+    expires: string;
+  }
+}
