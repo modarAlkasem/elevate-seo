195c35acc5d7d04b8993403176d0331fb4071511
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-10 23:34:59 +0200

    feat: Implement context for managing Auth Dialog state

--- frontend/src/wrappers/client-wrapper.tsx
+++ frontend/src/wrappers/client-wrapper.tsx
@@ -3,11 +3,13 @@
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 
+import { AuthDialogProvider } from "@/context/auth-dialog-context";
+
 export const ClientWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       <SessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
-        {children}
+        <AuthDialogProvider>{children}</AuthDialogProvider>
       </SessionProvider>
     </ThemeProvider>
   );
