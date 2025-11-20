17936361499db2237ce9771cb2f6473588a6f9c1
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-20 18:09:52 +0200

    feat (BE): implement ScrapingJobCreationSerializer serializer for validating create ScrapingJob object request's body

---
+++ backend/apps/scraping_jobs/serializers.py
@@ -0,0 +1,11 @@
+# Django Imports
+from django.core.validators import MinLengthValidator
+
+# REST Framework Imports
+from rest_framework import serializers
+
+
+class ScrapingJobCreationSerializer(serializers.Serializer):
+    prompt = serializers.CharField(min_length=2, max_length=255)
+    country_code = serializers.CharField(min_length=2, max_length=2)
+    existing_job_id = serializers.UUIDField(required=False)
