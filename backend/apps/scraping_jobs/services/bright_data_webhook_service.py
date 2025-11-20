5b1cf819a4a127b67c3f9cf2d9a76c951f4aea2d
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-21 00:51:56 +0200

    feat: handle SEO report's schema validation when fetching ScrapingJob in BrightData webhook

--- backend/apps/scraping_jobs/services/bright_data_webhook_service.py
+++ backend/apps/scraping_jobs/services/bright_data_webhook_service.py
@@ -11,6 +11,10 @@ from django.conf import settings
 from rest_framework import status
 from rest_framework.request import Request
 
+# Third-party Imports
+from pydantic import ValidationError
+
+
 # App Imports
 from ..models import ScrapingJob
 
@@ -22,40 +26,51 @@ class BrightDataWebhookService:
 
     @staticmethod
     async def handle(request: Request):
+        try:
 
-        data = request.data
-        signature = request.headers.get("X-BrightData-Signature")
-        secret: str = settings.BRIGHTDATA_WEBHOOK_SECRET
+            data = request.data
+            signature = request.headers.get("X-BrightData-Signature")
+            secret: str = settings.BRIGHTDATA_WEBHOOK_SECRET
 
-        computed = hmac.new(secret.encode(), request.body, hashlib.sha256).hexdigest()
+            computed = hmac.new(
+                secret.encode(), request.body, hashlib.sha256
+            ).hexdigest()
 
-        if not hmac.compare_digest(computed or "", signature):
-            logger.error("Unauthorized BrightData webhook access", extra={"data": data})
-            return (
-                "Unauthorized to do this action",
-                "FORBIDDEN",
-                status.HTTP_403_FORBIDDEN,
+            if not hmac.compare_digest(computed or "", signature):
+                logger.error(
+                    "Unauthorized BrightData webhook access", extra={"data": data}
+                )
+                return (
+                    "Unauthorized to do this action",
+                    "FORBIDDEN",
+                    status.HTTP_403_FORBIDDEN,
+                )
+
+            job_id = request.query_params.get("job-id")
+            if not job_id:
+                logger.error("No job ID found with Webhook URL", extra={"data": data})
+                return (
+                    "No job ID found",
+                    "BAD_REQUEST",
+                    status.HTTP_400_BAD_REQUEST,
+                )
+
+            job = await ScrapingJob.objects.get_job_by_id(job_id)
+            if not job:
+                logger.error("No job found for job ID", extra={"job_id": job_id})
+                return "No job found for job ID", "NOT_FOUND", status.HTTP_404_NOT_FOUND
+
+            if not isinstance(data, list):
+                data = [data]
+
+            await ScrapingJob.objects.save_raw_scraping_data(job_id, data)
+
+            # Here where should schedule AI analysis as Celery task
+
+            return None, "SUCCESS", status.HTTP_200_OK
+
+        except ValidationError as e:
+            logger.error(
+                "SEO report's schema validation falied", extra={"errors": e.errors()}
             )
-
-        job_id = request.query_params.get("job-id")
-        if not job_id:
-            logger.error("No job ID found with Webhook URL", extra={"data": data})
-            return (
-                "No job ID found",
-                "BAD_REQUEST",
-                status.HTTP_400_BAD_REQUEST,
-            )
-
-        job = await ScrapingJob.objects.get_job_by_id(job_id)
-        if not job:
-            logger.error("No job found for job ID", extra={"job_id": job_id})
-            return "No job found for job ID", "NOT_FOUND", status.HTTP_404_NOT_FOUND
-
-        if not isinstance(data, list):
-            data = [data]
-
-        await ScrapingJob.objects.save_raw_scraping_data(job_id, data)
-
-        # Here where should schedule AI analysis as Celery task
-
-        return None, "SUCCESS", status.HTTP_200_OK
+            return e.errors(), "UNKNOWN_ERROR", status.HTTP_500_INTERNAL_SERVER_ERROR
--- backend/apps/scraping_jobs/services/scraping_job_service.py
+++ backend/apps/scraping_jobs/services/scraping_job_service.py
@@ -11,4 +11,27 @@ from ..models import ScrapingJob
 
 
 class ScrapingJobService:
-    pass
+
+    @staticmethod
+    async def create(request: Request) -> Response:
+        data = request.data
+        user = request.user
+
+        serializer = ScrapingJobCreationSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            validated_data = serializer.validated_data
+            existing_job_id = validated_data.get("existing_job_id")
+
+            if existing_job_id:
+                retry_info = await ScrapingJob.objects.can_use_smart_retry(
+                    existing_job_id, user.id
+                )
+
+                if retry_info.get("can_retry_analysis_only", False):
+                    # Smart retry's logic goes here
+                    pass
+
+            else:
+                scraping_job = await ScrapingJob.objects.acreate(
+                    user=user, original_prompt=validated_data.get("original_prompt")
+                )
