55ceac74c65a3da2e9ec8983f227e4f534f09080
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-24 21:43:58 +0200

    feat: Implement Analysis prompts forLLM

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -6,7 +6,15 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-app = Celery("elevate_seo")
+
+class CustomCelery(Celery):
+    def gen_task_name(self, name: str, module: str):
+        if module.endswith("tasks"):
+            module = module[:-6]
+        return super().gen_task_name(name, module)
+
+
+app = CustomCelery("elevate_seo")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks()
