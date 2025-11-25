4334cbfeaab949d22461a8988b5bdd23d919f8cf
mudarAlkasem <modarAlkasem@gmail.com>
2025-11-25 19:26:16 +0200

    feat: Implement Websocket consumer for gettingScrapingJob instance's updates in real-time

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -9,17 +9,32 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 # Python Imports
 import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+sys.path.append(os.path.join(BASE_DIR, "apps"))
 
 # Django Imports
 from django.core.asgi import get_asgi_application
 
 # Third Party Imports
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# Project Imports
+from scraping_jobs.routing import websocket_patterns
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
+# Project Imports
 
-application = ProtocolTypeRouter({"http": django_asgi_app})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(websocket_patterns),
+    }
+)
