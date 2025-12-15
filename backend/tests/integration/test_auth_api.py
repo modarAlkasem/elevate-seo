86357da190c3b885654f0090329044286896cb66
mudarAlkasem <modarAlkasem@gmail.com>
2025-12-15 23:16:01 +0200

    test: Update to test_authenticate_api
    - Define test function for signup fails with duplicate email

--- backend/tests/integration/test_auth_api.py
+++ backend/tests/integration/test_auth_api.py
@@ -17,14 +17,33 @@ class TestAuthenticationAPI:
     """Test Authentication Endpoints"""
 
     def test_signup_success(self, api_client: APIClient):
-        """Test successful user registeration using credentials"""
+        """Test successful user registeration using credentials method"""
 
         url = reverse("authentication:signup")
 
-        data = {"email": "newuser@test.com", "password": "SecurePassword!"}
+        data = {
+            "name": "Test User",
+            "email": "newuser@test.com",
+            "password": "SecurePassword!",
+        }
 
         response = api_client.post(url, data, format="json")
 
         assert status.HTTP_201_CREATED == response.status_code
         assert "email" in response.data
         assert response.data.get("email") == data.get("email")
+
+    def test_signup_duplicate_email(self, api_client):
+        """Test signup falis with duplicate email"""
+
+        url = reverse("authentication:signup")
+
+        data = {
+            "name": "Test User",
+            "email": "newuser@test.com",
+            "password": "SecurePassword!",
+        }
+
+        response = api_client.post(url, data, format="json")
+
+        assert status.HTTP_400_BAD_REQUEST == response.status_code
